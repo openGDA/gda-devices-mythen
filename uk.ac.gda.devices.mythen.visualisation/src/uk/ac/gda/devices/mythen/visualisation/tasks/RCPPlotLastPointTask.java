3799374523731de9778245654efa7d5b9c57f374
Peter Chang <peter.chang@diamond.ac.uk>
2016-07-06 11:44:14 +0100

    Refactor to new January namespace
    Change-Id: I629322fabaa169218c3f339f6166958b72879af7

--- uk.ac.gda.devices.mythen.visualisation/src/uk/ac/gda/devices/mythen/visualisation/tasks/RCPPlotLastPointTask.java
+++ uk.ac.gda.devices.mythen.visualisation/src/uk/ac/gda/devices/mythen/visualisation/tasks/RCPPlotLastPointTask.java
@@ -21,8 +21,8 @@ package uk.ac.gda.devices.mythen.visualisation.tasks;
 import java.io.File;
 
 import org.apache.commons.io.FilenameUtils;
-import org.eclipse.dawnsci.analysis.api.dataset.IDataset;
-import org.eclipse.dawnsci.analysis.dataset.impl.DatasetFactory;
+import org.eclipse.january.dataset.DatasetFactory;
+import org.eclipse.january.dataset.IDataset;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.InitializingBean;
